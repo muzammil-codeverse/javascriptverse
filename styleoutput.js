5a69340588f8f86300a9d7d104be0d513a4599cb
muzammil-codeverse <muzammilcodeverse@gmail.com>
2024-08-06 08:01:39 +0500

    variables

---
+++ styleoutput.js
@@ -0,0 +1,2 @@
+console.log("%cHello, World","color: blue; font-size: 24px; font-weight: bold;");
+// Displays in the browser
---
+++ variables.js
@@ -0,0 +1,10 @@
+let petDog = "Rex";
+let petCat = "Pepper";
+console.log("My Pet Dog's Name >> "+petCat)
+console.log("My Pet Cat's Name >> "+petDog)
+catSound = "purr"
+dogSound = "woof"
+console.log(petCat+" says: "+catSound)
+console.log(petDog+" says: "+dogSound)
+catSound = "meow"
+console.log(petCat+" says: "+catSound)
